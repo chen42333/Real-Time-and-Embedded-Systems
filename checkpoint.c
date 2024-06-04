9b8fd53c299f9fa69cdbea72e45dc6ba208df6d7
RTOS vm <me@nella17.tw>
2024-06-04 17:37:44 +0800

    lab5 noinit

--- checkpoint.c
+++ checkpoint.c
@@ -5,26 +5,24 @@
 #define UCHEAP_SIZE configTOTAL_HEAP_SIZE
 #define RAM_SIZE 0x0800
 #define RAM_ADDR 0x1C00
-#define RAM_LOW_SIZE (0x2E0 + 0x230)
-// #define RAM_HIGH_SIZE (0x400)
 
 extern uint8_t ucHeap[ configTOTAL_HEAP_SIZE ];
 
-#pragma PERSISTENT(backup_ucHeap)
-uint8_t backup_ucHeap[2][UCHEAP_SIZE] = {0};
+#pragma NOINIT(backup_ucHeap)
+uint8_t backup_ucHeap[2][UCHEAP_SIZE];
 
 #pragma PERSISTENT(backup_idx)
 uint8_t backup_idx = 2;
 
 uint32_t cur_register[16] = {0};
 
-#pragma PERSISTENT(backup_register)
-uint32_t backup_register[2][16] = {0};
+#pragma NOINIT(backup_register)
+uint32_t backup_register[2][16];
 
 uint8_t* ram = RAM_ADDR;
 
-#pragma PERSISTENT(backup_ram_low)
-uint8_t backup_ram_low[2][RAM_LOW_SIZE] = {0};
+#pragma NOINIT(backup_ram)
+uint8_t backup_ram[2][RAM_SIZE];
 
 #include "inc/hw_regaccess.h"
 #include "inc/hw_memmap.h"
@@ -49,8 +47,8 @@ void commit_c() {
         backup_register[cur_w_idx][i] = cur_register[i];
     for (i = 0; i < UCHEAP_SIZE; i++)
         backup_ucHeap[cur_w_idx][i] = ucHeap[i];
-    for (i = 0; i < RAM_LOW_SIZE; i++)
-        backup_ram_low[cur_w_idx][i] = ram[i];
+    for (i = 0; i < RAM_SIZE; i++)
+        backup_ram[cur_w_idx][i] = ram[i];
 
     backup_idx = cur_w_idx;
     commit_leave();
@@ -66,8 +64,8 @@ void restore_c() {
         cur_register[i] = backup_register[backup_idx][i];
     for (i = 0; i < UCHEAP_SIZE; i++)
         ucHeap[i] = backup_ucHeap[backup_idx][i];
-    for (i = 0; i < RAM_LOW_SIZE; i++)
-        ram[i] = backup_ram_low[backup_idx][i];
+    for (i = 0; i < RAM_SIZE; i++)
+        ram[i] = backup_ram[backup_idx][i];
 
     restore_leave();
 }
